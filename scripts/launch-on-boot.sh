b787da363c15eb6f110c7b093d523049c194b03d
Goran Blažič <goranche@me.com>
2025-08-13 16:44:15 +0200

    Add launchd plist and script for launching at boot

---
+++ scripts/launch-on-boot.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Start container system, iterate over containers and start the ones
+# that have a label named "restart" with the value "boot".
+
+error() {
+	>&2 echo $1
+	exit 1
+}
+
+# TODO: find a better way of finding the container command.
+# Brew puts it in /usr/local/bin (as does the Makefile by default, I believe), which is not on the path for launchd
+
+# Some sanity checks
+
+command -v /usr/local/bin/container >/dev/null 2>&1 || error "container is not installed at /usr/local/bin"
+command -v jq >/dev/null 2>&1 || error "jq is not installed"
+
+# Make sure the container system is up and running
+/usr/local/bin/container system start
+
+# Just in case, not sure it's needed
+sleep 3
+
+for id in $(/usr/local/bin/container list --all --format json | jq -r '[ .[] | select(.configuration.labels | has("restart")) | select(.configuration.labels.restart == "boot" ) | .configuration.id ] | to_entries[] | "\(.value)"'); do
+	/usr/local/bin/container start $id
+done
